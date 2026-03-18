4ed1e07f1c9f13065c24eee46da01afb97c16da2
Nikesh <nikeshnikesh520@gmail.com>
2026-03-18 11:11:00 +0530

    New Updated code

--- src/main/java/Counter.java
+++ src/main/java/Counter.java
@@ -1,17 +1,29 @@
-import java.io.*;
-import javax.servlet.*;
-import javax.servlet.http.*;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 public class Counter extends HttpServlet {
-    int accesses = 0;
 
-    public void doGet(HttpServletRequest request, HttpServletResponse response)
-         throws IOException, ServletException
-    {
+    private static final long serialVersionUID = 1L;
+
+    private int accesses = 0;
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
         response.setContentType("text/html");
+
         PrintWriter out = response.getWriter();
 
         accesses++;
-        out.print("Number of times this servlet has been accessed:" + accesses);
+
+        out.println("<html><body>");
+        out.println("<h2>Number of times this servlet has been accessed: " + accesses + "</h2>");
+        out.println("</body></html>");
     }
-} 
+}
